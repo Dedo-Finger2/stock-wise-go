c7ce15c6afc0adf6f4d1515b2fe1e16a5720a80e
antonio mauricio <antonioimportant@gmail.com>
2024-08-23 11:30:20 -0300

    feat: Makes sure you cannot have two users with the same email registered + testing

--- internal/services/register_user.go
+++ internal/services/register_user.go
@@ -1,11 +1,21 @@
 package services
 
 import (
+	"errors"
+
 	"github.com/Dedo-Finger2/stock-wise-go/internal/domain/entities"
 	"golang.org/x/crypto/bcrypt"
 )
 
 func (s *UserService) RegisterUser(email, password string) error {
+	userAlreadyExists, err := s.r.GetUserByEmail(email)
+	if err != nil {
+		return err
+	}
+	if userAlreadyExists.ID != "" {
+		return errors.New("email already in use. Log in or try to use another email")
+	}
+
 	cost := 14
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), cost)
 	if err != nil {
--- internal/services/register_user_unit_test.go
+++ internal/services/register_user_unit_test.go
@@ -10,17 +10,34 @@ import (
 )
 
 func Test_RegisterUser(t *testing.T) {
-	r := repositories.NewInMemoryUserRepository()
-	s := services.NewUserService(r)
-
 	t.Run("it should create a new user", func(t *testing.T) {
+		r := repositories.NewInMemoryUserRepository()
+		s := services.NewUserService(r)
+
 		err := s.RegisterUser("validemail@gmail.com", "1234567")
 		if err != nil {
 			t.Errorf("failed to register a new user: %s", err)
 		}
 	})
 
+	t.Run("it should return an error on trying to register two users with the same email", func(t *testing.T) {
+		r := repositories.NewInMemoryUserRepository()
+		s := services.NewUserService(r)
+
+		err := s.RegisterUser("validemail@gmail.com", "1234567")
+		if err != nil {
+			t.Errorf("failed to register a new user: %s", err)
+		}
+		err = s.RegisterUser("validemail@gmail.com", "12342323567")
+		if err == nil {
+			t.Error("it was expected to return an error, but got nil.")
+		}
+	})
+
 	t.Run("it should be hashing the user password", func(t *testing.T) {
+		r := repositories.NewInMemoryUserRepository()
+		s := services.NewUserService(r)
+
 		err := s.RegisterUser("validemail@gmail.com", "1234567")
 		if err != nil {
 			t.Errorf("failed to register a new user: %s", err)
